89fd52e14af39898fbcc7a61c17591f7a8bd413a
Richard <programmingislife23@gmail.com>
2024-02-29 17:53:14 -0300

    conseguindo mudar style com dom manipulation

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const links = document.querySelectorAll("a");
 const titulo = document.querySelector("#titulo");
+const paragrafo = document.querySelector(".paragrafo");
+
+const paragrafoRed = (paragrafo.style.color = "red");
+const paragrafoBig = (paragrafo.style.fontSize = "2em");
+console.log(`Paragrafo no HTML: ${paragrafo.innerText}`);
 console.log(`Testando o titulo pegando pelo id: ${titulo.innerText}`);
 console.log(`Esses são os links da página, todos eles: ${links}`);
 for (let link of links) {
